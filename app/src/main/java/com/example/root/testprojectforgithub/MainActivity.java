ae3d926905d45a7940a62281e8c3fc1633aaf87e
Bilel Methnani <bmethnani@anypli.com>
2015-08-25 16:35:46 +0100

    Added a button to the main view

--- app/src/main/java/com/example/root/testprojectforgithub/MainActivity.java
+++ app/src/main/java/com/example/root/testprojectforgithub/MainActivity.java
@@ -4,13 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mbutton = (Button) findViewById(R.id.button);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),"this is spata !!",Toast.LENGTH_LONG);
+            }
+        });
     }
 
     @Override
